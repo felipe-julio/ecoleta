4e45b7af26de440896ff25f2e6fe025d25554319
felipe alves julio <phelipejulio16@gmail.com>
2020-08-07 08:10:17 -0300

    criação do upload de imagens

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,5 +1,5 @@
-import knex from '../database/connection';
 import {Request, Response } from 'express';
+import knex from '../database/connection';
 
 class PointsController {
   async index(request: Request, response: Response){
@@ -17,6 +17,13 @@ class PointsController {
      .distinct()
      .select('points.*');
 
+     const serializedPoints = points.map(point => {
+      return {
+          ...points,
+          image_url: `http://192.168.0.106:3333/uploads/${point.image}`,
+      };
+     });
+
     return response.json(points);
   } 
 
@@ -29,12 +36,17 @@ class PointsController {
        return response.status(400).json({ message: 'Point not found.'});
     }
 
+    const serializedPoint = {
+          ...point,
+          image_url: `http://192.168.0.106:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
 
   }
 
@@ -42,7 +54,7 @@ class PointsController {
     const {
       name,
       email,
-      whatsApp,
+      whatsapp,
       latitude,
       longitude,
       city,
@@ -53,10 +65,10 @@ class PointsController {
     const trx = await knex.transaction();
   
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
-      whatsApp,
+      whatsapp,
       latitude,
       longitude,
       city,
@@ -67,7 +79,10 @@ class PointsController {
   
     const point_id = insertedIds[0];
   
-    const pointItems = items.map((item_id : number) => {
+    const pointItems = items
+     .split(',')
+     .map((item : string ) => Number(item.trim()))
+     .map((item_id : number) => {
         return {
             item_id,
             point_id
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,10 +6,12 @@ import api from '../../services/api'
 import axios from 'axios'
 import { LeafletMouseEvent, LeafletEvent } from 'leaflet'
 
+import Dropzone from '../../components/dropzone';
 
 import './styles.css'
 
 import logo from '../../assets/logo.svg'
+import { join } from 'path';
 
 // array ou objeto: manualmente informar o tipo da variavel
 
@@ -38,6 +40,7 @@ const CreatePoint = () => {
     const[selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
     const[initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
     const[selectedItems, setSelectedItems] = useState<number[]>([0,0]);
+    const[selectedFile, setSelectedFile] = useState<File>();
 
     const [formData, setFormData] = useState({
         name: '',
@@ -118,22 +121,27 @@ const CreatePoint = () => {
     async function handleSubmit(event: FormEvent){
         event.preventDefault();
 
+        console.log(selectedFile);
+
         const { name, email, whatsApp } = formData;
         const uf = selectedUf;
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsApp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsApp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile){
+            data.append('image', selectedFile);
+        }
 
         await api.post('points', data);
 
@@ -156,6 +164,8 @@ const CreatePoint = () => {
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do < br /> ponto de coleta</h1>
 
+                <Dropzone onFileUploaded={setSelectedFile}></Dropzone>
+                
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
